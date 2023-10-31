dd73b04e55c3e86f752dfaa5f04c3d9dee9b3b78
David Vanhoucke <dvanhoucke@redborder.com>
2023-10-31 17:57:11 +0000

    change access to node.redborder

--- resources/libraries/get_sensors_all_info.rb
+++ resources/libraries/get_sensors_all_info.rb
@@ -5,7 +5,7 @@ module Rb_proxy
         sensor_types = ["vault-sensor","flow-sensor","mse-sensor","scanner-sensor","meraki-sensor","ale-sensor", "device-sensor"]
   
         sensor_types.each do |s_type|
-          sensors = search(:node, "role:#{s_type} AND redborder_parent_id:#{node.redborder.sensor_id}").sort
+          sensors = search(:node, "role:#{s_type} AND redborder_parent_id:#{node["redborder"]["sensor_id"]}").sort
   
           sensors_info[s_type] = []
           sensors.each do |s|
