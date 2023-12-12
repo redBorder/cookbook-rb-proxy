62aff0be8c7f73ddcd4d6f4520ad61989fb98a2e
Miguel Negron <manegron@redborder.com>
2023-12-12 10:08:52 +0000

    Add configure_journald

--- resources/recipes/default.rb
+++ resources/recipes/default.rb
@@ -2,11 +2,11 @@
 # Cookbook Name:: proxy
 # Recipe:: default
 #
-# Copyright 2022, redborder
+# Copyright 2024, redborder
 #
 # AFFERO GENERAL PUBLIC LICENSE V3
 #
 
 include_recipe 'rb-proxy::prepare_system'
 include_recipe 'rb-proxy::configure'
-
+include_recipe 'rb-proxy::configure_journald'
