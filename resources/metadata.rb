eeff4be6043a057ed24800cd4335b9febb94a9a4
Miguel Negron <manegron@redborder.com>
2023-12-14 14:35:58 +0000

    release 0.1.1

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'manegron@redborder.com'
 license          'All rights reserved'
 description      'Installs/Configures redborder proxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'rb-selinux'
 depends 'zookeeper'
