1f129952d9ccc8fd79deec663612b329d60dd844
Luis Blanco <ljblanco@redborder.com>
2024-05-07 11:28:50 +0100

    exclude chef-client from memservice

--- resources/recipes/prepare_system.rb
+++ resources/recipes/prepare_system.rb
@@ -69,13 +69,4 @@ node.default["redborder"]["zookeeper"]["zk_hosts"] = "zookeeper.service:#{node["
 #getting total system memory less 10% reserved by system
 sysmem_total = (node["memory"]["total"].to_i * 0.90).to_i
 #node attributes related with memory are changed inside the function to have simplicity using recursivity
-memory_services(sysmem_total)
-
-
-
-
-
-
-
-
-
+memory_services(sysmem_total, ['chef-client']) # Don't assign memory to chef because the service will get handled
