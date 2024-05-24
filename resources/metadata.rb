26c317f5cbf57680c8e7ac760b26607363a70cc3
vimesa <vimesa@redborder.com>
2024-05-24 10:46:32 +0100

    Release 0.2.0

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'manegron@redborder.com'
 license          'All rights reserved'
 description      'Installs/Configures redborder proxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.6'
+version          '0.2.0'
 
 depends 'rb-common'
 depends 'rb-selinux'
