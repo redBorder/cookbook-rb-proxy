f1b18a303e2ef26c61e4a41c7adb874897d74127
Miguel Negron <manegron@redborder.com>
2024-06-09 01:03:53 +0100

    Fix: Update styles in recipes

--- resources/recipes/configure.rb
+++ resources/recipes/configure.rb
@@ -1,141 +1,189 @@
-#
-# Cookbook Name:: proxy
+# Cookbook:: proxy
 # Recipe:: configure
-#
-# Copyright 2024, redborder
-#
-# AFFERO GENERAL PUBLIC LICENSE V3
-#
+# Copyright:: 2024, redborder
+# License:: Affero General Public License, Version 3
+
 # Services configuration
 
-# proxy services
 proxy_services = proxy_services()
 
-rb_common_config "Configure common" do
+rb_common_config 'Configure common' do
   action :configure
 end
 
-rb_selinux_config "Configure Selinux" do
-  if shell_out("getenforce").stdout.chomp == "Disabled"
+rb_selinux_config 'Configure Selinux' do
+  if shell_out('getenforce').stdout.chomp == 'Disabled'
     action :remove
   else
     action :add
   end
 end
 
-zookeeper_config "Configure Zookeeper" do
-    port node["zookeeper"]["port"]
-    memory node["redborder"]["memory_services"]["zookeeper"]["memory"]
-    hosts [node.name]
-    action (proxy_services["zookeeper"] ? [:add] : [:remove])
+zookeeper_config 'Configure Zookeeper' do
+  port node['zookeeper']['port']
+  memory node['redborder']['memory_services']['zookeeper']['memory']
+  hosts [node.name]
+  if proxy_services['zookeeper']
+    action [:add]
+  else
+    action [:remove]
   end
-
-kafka_config "Configure Kafka" do
-    memory node["redborder"]["memory_services"]["kafka"]["memory"]
-    #maxsize node["redborder"]["manager"]["hd_services_current"]["kafka"]
-    managers_list [node.name]
-    zk_hosts node["redborder"]["zookeeper"]["zk_hosts"]
-    host_index node["redborder"]["kafka"]["host_index"]
-    action (proxy_services["kafka"] ? [:add] : [:remove])
 end
 
-geoip_config "Configure GeoIP" do
+kafka_config 'Configure Kafka' do
+  memory node['redborder']['memory_services']['kafka']['memory']
+  # maxsize node['redborder']['manager']['hd_services_current']['kafka']
+  managers_list [node.name]
+  zk_hosts node['redborder']['zookeeper']['zk_hosts']
+  host_index node['redborder']['kafka']['host_index']
+  if proxy_services['kafka']
+    action [:add]
+  else
+    action [:remove]
+  end
+end
+
+geoip_config 'Configure GeoIP' do
+  action :add
+end
+
+snmp_config 'Configure snmp' do
+  hostname node['hostname']
+  cdomain node['redborder']['cdomain']
+  if proxy_services['snmp']
     action :add
+  else
+    action :remove
+  end
 end
 
-snmp_config "Configure snmp" do
-    hostname node["hostname"]
-    cdomain node["redborder"]["cdomain"]
-    action (proxy_services["snmp"] ? :add : :remove)
-end
-  
-rbmonitor_config "Configure redborder-monitor" do
-    name node["hostname"]
-    action (proxy_services["redborder-monitor"] ? :add : :remove)
+rbmonitor_config 'Configure redborder-monitor' do
+  name node['hostname']
+  if proxy_services['redborder-monitor']
+    action :add
+  else
+    action :remove
+  end
 end
 
-rbscanner_config "Configure redborder-scanner" do
-    scanner_nodes node.run_state["sensors_info_all"]["scanner-sensor"]
-    action (proxy_services["redborder-scanner"] ? [:add] : [:remove])
-end
-  
-f2k_config "Configure f2k" do
-    sensors node.run_state["sensors_info"]["flow-sensor"]
-    action (proxy_services["f2k"] ? [:add] : [:remove])
-end
-
-pmacct_config "Configure pmacct" do
-    sensors node.run_state["sensors_info"]["flow-sensor"]
-    action (proxy_services["pmacct"] ? [:add] : [:remove])
+rbscanner_config 'Configure redborder-scanner' do
+  scanner_nodes node.run_state['sensors_info_all']['scanner-sensor']
+  if proxy_services['redborder-scanner']
+    action [:add]
+  else
+    action [:remove]
+  end
 end
 
-logstash_config "Configure logstash" do
-    cdomain node["redborder"]["cdomain"]
-    flow_nodes node.run_state["sensors_info_all"]["flow-sensor"]
-    namespaces node.run_state["namespaces"]
-    vault_nodes node.run_state["sensors_info_all"]["vault-sensor"]
-    device_nodes node.run_state["sensors_info_all"]["device-sensor"]
-    action (proxy_services["logstash"] ? [:add] : [:remove])
+f2k_config 'Configure f2k' do
+  sensors node.run_state['sensors_info']['flow-sensor']
+  if proxy_services['f2k']
+    action [:add]
+  else
+    action [:remove]
+  end
 end
 
-rsyslog_config "Configure rsyslog" do
-    vault_nodes node.run_state["sensors_info_all"]["vault-sensor"]
-    action (proxy_services["rsyslog"] ? [:add] : [:remove])
-end
-  
-rbnmsp_config "Configure redborder-nmsp" do
-    memory node["redborder"]["memory_services"]["redborder-nmsp"]["memory"]
-    flow_nodes node.run_state["sensors_info_all"]["flow-sensor"]
-    hosts node["redborder"]["zookeeper"]["zk_hosts"]
-    mode "proxy"
-    action (proxy_services["redborder-nmsp"] ? [:add, :configure_keys] : [:remove])
-end
-  
-n2klocd_config "Configure n2klocd" do
-    mse_nodes node.run_state["sensors_info_all"]["mse-sensor"]
-    meraki_nodes node.run_state["sensors_info_all"]["meraki-sensor"]
-    n2klocd_managers [node.name]
-    memory node["redborder"]["memory_services"]["n2klocd"]["memory"]
-    action (proxy_services["n2klocd"] ? [:add] : [:remove])
+pmacct_config 'Configure pmacct' do
+  sensors node.run_state['sensors_info']['flow-sensor']
+  if proxy_services['pmacct']
+    action [:add]
+  else
+    action [:remove]
+  end
 end
 
-rb_exporter_config "Configure rb-exporter" do
-    action (proxy_services["redborder-exporter"] ? [:add] : [:remove])
+logstash_config 'Configure logstash' do
+  cdomain node['redborder']['cdomain']
+  flow_nodes node.run_state['sensors_info_all']['flow-sensor']
+  namespaces node.run_state['namespaces']
+  vault_nodes node.run_state['sensors_info_all']['vault-sensor']
+  device_nodes node.run_state['sensors_info_all']['device-sensor']
+  if proxy_services['logstash']
+    action [:add]
+  else
+    action [:remove]
+  end
 end
 
-# TODO: replace node["redborder"]["services"] in action with "proxy_services".. 
-rbale_config "Configure redborder-ale" do
-    ale_nodes node.run_state["sensors_info_all"]["ale-sensor"]
-    action (node["redborder"]["services"]["redborder-ale"] ? [:add] : [:remove])
+rsyslog_config 'Configure rsyslog' do
+  vault_nodes node.run_state['sensors_info_all']['vault-sensor']
+  if proxy_services['rsyslog']
+    action [:add]
+  else
+    action [:remove]
+  end
 end
 
-k2http_config "Configure k2http" do
-    action (proxy_services["k2http"] ? [:add] : [:remove])
+rbnmsp_config 'Configure redborder-nmsp' do
+  memory node['redborder']['memory_services']['redborder-nmsp']['memory']
+  flow_nodes node.run_state['sensors_info_all']['flow-sensor']
+  hosts node['redborder']['zookeeper']['zk_hosts']
+  mode 'proxy'
+  if proxy_services['redborder-nmsp']
+    action [:add, :configure_keys]
+  else
+    action [:remove]
+  end
 end
 
-rbcgroup_config "Configure cgroups" do
-  action :add 
-end
-  
-#--------------------------MOTD--------------------------#
-
-manager =`grep "cloud_address" /etc/redborder/rb_init_conf.yml | cut -d' ' -f2`
-
-template "/etc/motd" do
-    source "motd.erb"
-    owner "root"
-    group "root"
-    mode 0644
-    retries 2
-    variables(:manager_info => node["redborder"]["cdomain"], :manager => manager)
+n2klocd_config 'Configure n2klocd' do
+  mse_nodes node.run_state['sensors_info_all']['mse-sensor']
+  meraki_nodes node.run_state['sensors_info_all']['meraki-sensor']
+  n2klocd_managers [node.name]
+  memory node['redborder']['memory_services']['n2klocd']['memory']
+  if proxy_services['n2klocd']
+    action [:add]
+  else
+    action [:remove]
+  end
 end
 
-## TODO: replace node["redborder"]["services"] in action with "proxy_services".. 
-#freeradius_config "Configure radiusd" do
-#    flow_nodes node.run_state["sensors_info_all"]["flow-sensor"]
-#    mode "proxy"
-#    action (node["redborder"]["services"]["radiusd"] ? [:config_common] : [:remove])
-#end
+rb_exporter_config 'Configure rb-exporter' do
+  if proxy_services['redborder-exporter']
+    action [:add]
+  else
+    action [:remove]
+  end
+end
 
+# TODO: replace node['redborder']['services'] in action with 'proxy_services'..
+rbale_config 'Configure redborder-ale' do
+  ale_nodes node.run_state['sensors_info_all']['ale-sensor']
+  if node['redborder']['services']['redborder-ale']
+    action [:add]
+  else
+    action [:remove]
+  end
+end
 
+k2http_config 'Configure k2http' do
+  if proxy_services['k2http']
+    action [:add]
+  else
+    action [:remove]
+  end
+end
 
+rbcgroup_config 'Configure cgroups' do
+  action :add
+end
+
+# MOTD
+manager = `grep 'cloud_address' /etc/redborder/rb_init_conf.yml | cut -d' ' -f2`
+
+template '/etc/motd' do
+  source 'motd.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  retries 2
+  variables(manager_info: node['redborder']['cdomain'], manager: manager)
+end
+
+# TODO: replace node['redborder']['services'] in action with 'proxy_services'..
+# freeradius_config 'Configure radiusd' do
+#   flow_nodes node.run_state['sensors_info_all']['flow-sensor']
+#   mode 'proxy'
+#  action (node['redborder']['services']['radiusd'] ? [:config_common] : [:remove])
+# end
--- resources/recipes/prepare_system.rb
+++ resources/recipes/prepare_system.rb
@@ -1,72 +1,67 @@
-#
-# Cookbook Name:: proxy
+# Cookbook:: proxy
 # Recipe:: prepare_system
-#
-# Copyright 2024, redborder
-#
-# AFFERO GENERAL PUBLIC LICENSE V3
-#
-extend Rb_proxy::Helpers
+# Copyright:: 2024, redborder
+# License:: Affero General Public License, Version 3
 
-#clean metadata to get packages upgrades
-execute "Clean yum metadata" do
-  command "yum clean metadata"
+extend RbProxy::Helpers
+
+# clean metadata to get packages upgrades
+execute 'Clean yum metadata' do
+  command 'yum clean metadata'
 end
 
 # Stop rb-register
-if File.exist?"/etc/redborder/sensor-installed.txt"
-  service "rb-register" do
+if File.exist?('/etc/redborder/sensor-installed.txt')
+  service 'rb-register' do
     ignore_failure true
     action [:disable, :stop]
   end
 end
 
-#Configure and enable chef-client
-dnf_package "redborder-chef-client" do
+# Configure and enable chef-client
+dnf_package 'redborder-chef-client' do
   flush_cache [:before]
   action :upgrade
 end
 
-template "/etc/sysconfig/chef-client" do
-  source "sysconfig_chef-client.rb"
-  mode 0644
+template '/etc/sysconfig/chef-client' do
+  source 'sysconfig_chef-client.rb.erb'
+  mode '0644'
   variables(
-    :interval => node["chef-client"]["interval"],
-    :splay => node["chef-client"]["splay"],
-    :options => node["chef-client"]["options"]
+    interval: node['chef-client']['interval'],
+    splay: node['chef-client']['splay'],
+    options: node['chef-client']['options']
   )
 end
 
-if node["redborder"]["services"]["chef-client"]
-  service "chef-client" do
+service 'chef-client' do
+  if node['redborder']['services']['chef-client']
     action [:enable, :start]
-  end
-else
-  service "chef-client" do
+  else
     action [:stop]
   end
 end
 
-cdomain = "redborder.cluster"
-#File.open('/etc/redborder/cdomain') {|f| cdomain = f.readline.chomp}
-node.default["redborder"]["cdomain"] = cdomain
+cdomain = 'redborder.cluster'
+# File.open('/etc/redborder/cdomain') {|f| cdomain = f.readline.chomp}
+node.default['redborder']['cdomain'] = cdomain
 
-#get sensors info
-node.run_state["sensors_info"] = get_sensors_info()
+# get sensors info
+node.run_state['sensors_info'] = get_sensors_info()
 
-#get sensors info full info
-node.run_state["sensors_info_all"] = get_sensors_all_info()
+# get sensors info full info
+node.run_state['sensors_info_all'] = get_sensors_all_info()
 
-#get namespaces
-node.run_state["namespaces"] = get_namespaces
+# get namespaces
+node.run_state['namespaces'] = get_namespaces
 
-#get string with all zookeeper hosts and port separated by commas, its needed for multiples services
-zk_port = node["redborder"]["zookeeper"]["port"]
-#zk_hosts = ["localhost"].map {|z| "#{z}.node:#{zk_port}"}.join(',')
-node.default["redborder"]["zookeeper"]["zk_hosts"] = "zookeeper.service:#{node["redborder"]["zookeeper"]["port"]}"
+# get string with all zookeeper hosts and port separated by commas, its needed for multiples services
+# zk_port = node['redborder']['zookeeper']['port']
+# zk_hosts = ['localhost'].map {|z| '#{z}.node:#{zk_port}'}.join(',')
+node.default['redborder']['zookeeper']['zk_hosts'] = "zookeeper.service:#{node['redborder']['zookeeper']['port']}"
 
-#memory
-#getting total system memory less 10% reserved by system
-sysmem_total = (node["memory"]["total"].to_i * 0.90).to_i
-#node attributes related with memory are changed inside the function to have simplicity using recursivity
+# memory
+# getting total system memory less 10% reserved by system
+sysmem_total = (node['memory']['total'].to_i * 0.90).to_i
+# node attributes related with memory are changed inside the function to have simplicity using recursivity
 memory_services(sysmem_total)
