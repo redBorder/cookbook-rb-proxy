e1272bb644533de707c2e2cfbe5c2b94cae8cc84
nilsver <nverschaeve@redborder.com>
2024-06-20 14:18:09 +0100

    add check for monitor configuration pipelines

--- resources/libraries/get_pipelines.rb
+++ resources/libraries/get_pipelines.rb
@@ -3,11 +3,14 @@ module RbProxy
     def get_pipelines
       logstash_pipelines = []
       sensors = get_sensors_info()
+      proxy_config = get_monitor_configuration()
+      has_device_sensors = !sensors['device-sensor'].nil? && !sensors['device-sensor'].empty?
+      contains_thermal = proxy_config.include?('thermal')
+      contains_bulkstats = proxy_config.include?('bulkstats_schema')
 
-      if proxy_services['logstash']
-        logstash_pipelines.push('redfish-pipeline') unless sensors['device-sensor'].empty?
-        logstash_pipelines.push('bulkstats-pipeline') unless sensors['device-sensor'].empty?
-      end
+      logstash_pipelines.push('redfish-pipeline') if has_device_sensors && contains_thermal
+      logstash_pipelines.push('bulkstats-pipeline') if has_device_sensors && contains_bulkstats
+
       logstash_pipelines
     end
   end
