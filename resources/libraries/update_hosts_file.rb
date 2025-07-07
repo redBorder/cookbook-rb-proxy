718733f20efd68d81d6ccf76475ae005bfe57ec8
ljblancoredborder <ljblanco@redborder.com>
2025-07-07 11:12:08 +0100

    documentation, optimization and lint

--- resources/libraries/update_hosts_file.rb
+++ resources/libraries/update_hosts_file.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# Helper module for rb-proxy cookbook
+# Contains functions to discover and build DNS resolution for /etc/hosts file
+#
 module RbProxy
   module Helpers
     require 'resolv'
@@ -10,6 +15,7 @@ module RbProxy
       hosts_hash = Hash.new { |hash, key| hash[key] = [] }
       File.readlines('/etc/hosts').each do |line|
         next if line.strip.empty? || line.start_with?('#')
+
         values = line.split(/\s+/)
         ip = values.shift
         services = values
@@ -53,7 +59,7 @@ module RbProxy
       implicit_services << "erchef.#{cdomain}" if cdomain
       # Services not contained in node information
       other_services = if cdomain
-                         %w(data http2k rbookshelf.s3).map { |s| "#{s}.#{cdomain}" }
+                         %w[data http2k rbookshelf.s3].map { |s| "#{s}.#{cdomain}" }
                        else
                          []
                        end
@@ -76,12 +82,12 @@ module RbProxy
     def add_virtual_ips_info(hosts_info, manager_registration_ip, cdomain)
       is_mode_manager = !node['redborder']['cloud']
       grouped_virtual_ips.each do |ip, services|
+        next if ip == '127.0.0.1'
+
         services.uniq! # Avoids having duplicate services in the list
         services.each do |service|
           # Add running services to localhost
-          if ip == '127.0.0.1'
-            next
-          elsif ip && is_mode_manager
+          if ip && is_mode_manager
             hosts_info[ip] = {} unless hosts_info[ip] # Create if necessary
             hosts_info[ip]['services'] = [] unless hosts_info[ip]['services'] # Create if necessary
             hosts_info[ip]['services'] << "#{service}.service"
@@ -111,7 +117,7 @@ module RbProxy
     def manager_to_ip(str)
       ipv4_regex = /\A(\d{1,3}\.){3}\d{1,3}\z/
       ipv6_regex = /\A(?:[A-Fa-f0-9]{1,4}:){7}[A-Fa-f0-9]{1,4}\z/
-      dns_regex  = /\A[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\z/
+      dns_regex  = /\A[a-zA-Z0-9-]+\.[a-zA-Z0-9\-.]+\z/
 
       return str if str.match?(ipv4_regex) || str.match?(ipv6_regex)
       return Resolv.getaddress(str).to_s if str.match?(dns_regex)
