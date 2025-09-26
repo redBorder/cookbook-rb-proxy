ea92cc59b7cf4340beaacee1d7fb5f0539ef2b3d
ljblancoredborder <ljblanco@redborder.com>
2025-09-26 13:40:00 +0100

    lint

--- resources/libraries/get_nodes.rb
+++ resources/libraries/get_nodes.rb
@@ -3,11 +3,10 @@ module RbProxy
     # Format of nodes differs from roles, in this case, we extract the info in node format
     def get_nodes(sensor_types = ['flow-sensor'])
       proxy_id = node['redborder']['sensor_id']
-      out = sensor_types.each_with_object({}) do |s_type, sensors_info|
+      sensor_types.each_with_object({}) do |s_type, sensors_info|
         sensors = search(:node, "role:#{s_type} AND redborder_parent_id:#{proxy_id}").sort
         sensors_info[s_type] = sensors
       end
-      out
     end
   end
 end
