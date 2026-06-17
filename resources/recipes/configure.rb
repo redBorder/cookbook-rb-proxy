a6d09c2e413b38a1a62002b148c2cfa6856465fe
manegron <manegron@redborder.com>
2026-06-17 13:58:36 +0100

    First version rb-proxy-net-tools

--- resources/recipes/configure.rb
+++ resources/recipes/configure.rb
@@ -101,6 +101,17 @@ rbscanner_config 'Configure redborder-scanner' do
   end
 end
 
+rb_proxy_net_tools_config 'Configure rb-proxy-net-tools' do
+  cdomain     node['redborder']['cdomain']
+  sensor_uuid node['redborder']['sensor_uuid']
+  rb_webui    "webui.#{node['redborder']['cdomain']}"
+  if proxy_services['rb-proxy-net-tools']
+    action [:add]
+  else
+    action [:remove]
+  end
+end
+
 f2k_config 'Configure f2k' do
   sensors node.run_state['sensors_info']['flow-sensor']
   if proxy_services['f2k']
