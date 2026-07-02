ae359085296caa3d44c345bd95b8fa3088d996bd
Nils <nverschaeve@redborder.com>
2026-07-02 15:09:50 +0100

    make exsi sensors work with redborder-monitor in proxy

--- resources/libraries/get_nodes.rb
+++ resources/libraries/get_nodes.rb
@@ -5,13 +5,11 @@ module RbProxy
       proxy_id = node['redborder']['sensor_id']
       if sensor_type == 'vmware-exsi-vm-sensor'
         hosts = search(:node, "role:vmware-exsi-sensor AND redborder_parent_id:#{proxy_id}")
-        host_ids = hosts.map { |h| h['redborder'] && h['redborder']['sensor_id'] }.compact
-        if host_ids.empty?
-          []
-        else
-          query_str = host_ids.map { |id| "redborder_parent_id:#{id}" }.join(' OR ')
-          search(:node, "role:vmware-exsi-vm-sensor AND (#{query_str})").sort
-        end
+        host_names = hosts.map(&:name)
+        search(:node, "role:vmware-exsi-vm-sensor").select do |vm|
+          parent_id = vm.dig('redborder', 'parent_id')
+          host_names.include?("rbvmware-exsi-#{parent_id}")
+        end.sort
       else
         search(:node, "role:#{sensor_type} AND redborder_parent_id:#{proxy_id}").sort
       end
--- resources/libraries/get_sensors_all_info.rb
+++ resources/libraries/get_sensors_all_info.rb
@@ -2,19 +2,17 @@ module RbProxy
   module Helpers
     def get_sensors_all_info
       sensors_info = {}
-      sensor_types = %w(vault-sensor flow-sensor mse-sensor scanner-sensor meraki-sensor ale-sensor device-sensor snmp-sensor redfish-sensor ipmi-sensor http_agent-sensor vmware_exsi-sensor vmware_exsi_vm-sensor)
+      sensor_types = %w(vault-sensor flow-sensor mse-sensor scanner-sensor meraki-sensor ale-sensor device-sensor snmp-sensor redfish-sensor ipmi-sensor http_agent-sensor vmware-exsi-sensor vmware-exsi-vm-sensor)
 
       sensor_types.each do |s_type|
         sensors =
           if s_type == 'vmware-exsi-vm-sensor'
             hosts = search(:node, "role:vmware-exsi-sensor AND redborder_parent_id:#{node['redborder']['sensor_id']}")
-            host_ids = hosts.map { |h| h['redborder'] && h['redborder']['sensor_id'] }.compact
-            if host_ids.empty?
-              []
-            else
-              query_str = host_ids.map { |id| "redborder_parent_id:#{id}" }.join(' OR ')
-              search(:node, "role:vmware-exsi-vm-sensor AND (#{query_str})").sort
-            end
+            host_names = hosts.map(&:name)
+            search(:node, "role:vmware-exsi-vm-sensor").select do |vm|
+              parent_id = vm.dig('redborder', 'parent_id')
+              host_names.include?("rbvmware-exsi-#{parent_id}")
+            end.sort
           else
             search(:node, "role:#{s_type} AND redborder_parent_id:#{node['redborder']['sensor_id']}").sort
           end
